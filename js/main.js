2af6fb9428da3809d897e31b4b4ec9cff3d538e6
wildflower2 <e.wythe@outlook.com>
2024-11-06 15:45:16 +0000

    a

--- js/main.js
+++ js/main.js
@@ -19,4 +19,25 @@ document.getElementById('myTestBtn').addEventListener('click', function() {
     console.info('hi');
 });
 
+let imageAr = [
+    'images/view1.jpg',
+    'images/view2.jpg',
+    'images/view3.jpg',
+    'images/view4.jpg',
+    'images/view5.jpg',
+    'images/view6.jpg'
+];
+
+let currentIndex = 0;
+
+function chgImage() {
+  currentIndex++;
+  if (currentIndex >= imageAr.length) {
+    currentIndex = 0;
+  }
+  document.getElementById('myImages').setAttribute('src', imageAr[currentIndex]);
+}
+
+setInterval(chgImage, 2000);
+
 //colour picker
